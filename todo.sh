f6390cc6c521ac9150b5e2f5c3bbe26a47d1078c
Charlotte <smit-charlotte@outlook.com>
2025-03-07 17:39:34 +0100

    ajout intro

--- todo.sh
+++ todo.sh
@@ -1,2 +1,12 @@
 #!/bin/bask
+echo ""
+echo "Bienvenue sur ta to-do list !"
+echo "Que souhaites-tu faire ?"
+echo ""
+echo "1. Ajouter une nouvelle tâche"
+echo "2. Retirer une tâche"
+echo "3. Afficher les tâches"
+echo ""
+echo "> entrer 1, 2 ou 3"
+read choix
 
