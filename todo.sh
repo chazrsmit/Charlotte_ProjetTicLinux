a045f49d91ad996231f202bcf558acda2b5535d0
Charlotte <smit-charlotte@outlook.com>
2025-03-07 18:06:56 +0100

    update action 1

--- todo.sh
+++ todo.sh
@@ -1,34 +1,34 @@
 #!/bin/bask
-echo "entre du texte"
-read texte
-echo $texte >> taches.txt
-# echo ""
-# echo "Bienvenue sur ta to-do list !"
-# echo "Que souhaites-tu faire ?"
-# echo ""
-# echo "1. Ajouter une nouvelle tâche"
-# echo "2. Retirer une tâche"
-# echo "3. Afficher les tâches"
-# echo ""
 
-# echo "Entre une action (indiquer 1, 2 ou 3) :"
-# read choix
+# echo "entre du texte"
+# read texte
+# echo $texte >> taches.txt
 
-# case $choix in
-#     1)
-#         echo "Entre la nouvelle tâche que tu souhaites ajouter :"
-#         # Actions pour l'option 1
-#         ;;
-#     2)
-#         echo "Quelle tâche souhaites-tu retirer ?"
-#         # Actions pour l'option 2
-#         ;;
-#     3)
-#         echo "Bye."
-#         # Actions pour l'option 3
-#         ;;
-#     *)
-#         echo "Option invalide. Veuillez entrer 1, 2 ou 3."
-#         # Actions pour une valeur invalide
-#         ;;
-# esac
+echo ""
+echo "Bienvenue sur ta to-do list !"
+echo "Que souhaites-tu faire ?"
+echo ""
+echo "1. Ajouter une nouvelle tâche"
+echo "2. Retirer une tâche"
+echo "3. Afficher les tâches"
+echo ""
+
+echo "Entre une action (indiquer 1, 2 ou 3) :"
+read choix
+
+case $choix in
+    1)
+        echo "Entre la nouvelle tâche que tu souhaites ajouter :"
+        read newtache
+        echo $newtache >> taches.txt #add numerotation avec $i après
+        ;;
+    2)
+        echo "Quelle tâche souhaites-tu retirer ?"
+        ;;
+    3)
+        echo "Bye."
+        ;;
+    *)
+        echo "Option invalide. Veuillez entrer 1, 2 ou 3."
+        ;;
+esac
